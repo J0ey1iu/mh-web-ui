aad1dc74fbd1e5bbf813ce517170d0679670aa49
j0ey1iu <joeyliu086@foxmail.com>
2026-07-20 14:22:53 +0800

    chore: update build mode from mhc to mh-local

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import vue from "@vitejs/plugin-vue"
 
 export default defineConfig(({ mode }) => {
   const isProd = mode === "production"
-  const isMhc = mode === "mhc"
+  const isMhc = mode === "mh-local"
   const apiPrefix = isMhc ? "" : "//localhost:8005"
   return {
     base: "./",
